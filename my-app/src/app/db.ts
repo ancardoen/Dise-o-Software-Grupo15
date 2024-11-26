cace191636f81e935af66c3e043733ea842fcbaf
Antonio Cardoen <ancardoen@alumnos.uai.cl>
2024-11-25 22:40:43 -0300

    mini update backend

--- my-app/src/app/db.ts
+++ my-app/src/app/db.ts
@@ -3,14 +3,31 @@ import "dotenv/config";
 
 const { Pool } = pg;
 
+if (
+  !process.env.DATABASE_HOST ||
+  !process.env.DATABASE_PORT ||
+  !process.env.DATABASE_NAME ||
+  !process.env.DATABASE_USER ||
+  !process.env.DATABASE_PASSWORD
+) {
+  throw new Error("Missing database configuration in environment variables");
+}
+
 const pool = new Pool({
   host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
+  port: Number(process.env.DATABASE_PORT),
   database: process.env.DATABASE_NAME,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
 });
 
 pool.connect();
+
+console.log({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+});
 
 export default pool;
